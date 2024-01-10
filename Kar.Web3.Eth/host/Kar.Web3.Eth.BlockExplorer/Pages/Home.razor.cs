c7c817bf38dc9ee5aa26143b8490cf02443a8194
SatishKarpine <satish@karpine.com>
2024-01-10 18:43:19 +0530

    Adminator Integration

---
+++ Kar.Web3.Eth/host/Kar.Web3.Eth.BlockExplorer/Pages/Home.razor.cs
@@ -0,0 +1,14 @@
+﻿namespace Kar.Web3.Eth.BlockExplorer.Pages
+{
+    public partial class Home
+    {
+        public List<DataModal> Data { get; set; }
+    }
+    public class DataModal
+    {
+        public int Index { get; set; }
+        public int Transactions { get; set; }
+        public DateTime DateTime { get; set; }
+        public int Size { get; set; }
+    }
+}
--- Kar.Web3.Eth/host/Kar.Web3.Eth.BlockExplorer/Program.cs
+++ Kar.Web3.Eth/host/Kar.Web3.Eth.BlockExplorer/Program.cs
@@ -1,10 +1,15 @@
 using Kar.Web3.Eth.BlockExplorer;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+//Register Syncfusion license 
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF1cWmhIfEx1RHxQdld5ZFRHallYTnNWUj0eQnxTdEZiW35dcHVRTmFeUkx+Wg==");
+builder.Services.AddSyncfusionBlazor();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
